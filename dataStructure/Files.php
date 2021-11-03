6930a62eb3062eb1759b0722e69ceeafaaf927c6
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-11-04 00:08:42 +0100

    new classes

--- dataStructure/Files.php
+++ dataStructure/Files.php
@@ -5,44 +5,42 @@ class Files extends MotherCell{
   protected $fileType = "";
   protected $extArray = array();
 
+  public function __construct($id, $name, $dirName, $size, $newFileType, $extArray){
+    parent :: __construct($id, $name, $dirName, $size);
+    $this->fileType=$newFileType;
+    $this->extArray=$extArray;
+  }
+
   public function getFileType(){
     return $this->fileType;
   }
-
   public function setFileType($newFileType){
     $this->fileType=$newFileType;
   }
+
   public function getExtArray(){
     return $this->extArray;
   }
-
   public function setExtArray($newExtArray){
     $this->extArray=$newExtArray;
   }
 
-  public function __construct($id, $name, $dirName, $size, $newFileType, $newExtArray){
-    parent :: __construct($id, $name, $dirName, $size);
-    $this->getFileType();
-    $this->getExtArray();
-    $this->setFileType($newFileType);
-    $this->setExtArray($newExtArray);
-  }
-
 }
+
+
 
 class ImageFile extends Files{
 
   protected $altString="";
 
-  public function __construct($id, $name, $dirName, $size, $newFileType, $newExtArray, $stringAlt){
+  public function __construct($id, $name, $dirName, $size, $newFileType, $newExtArray, $altString){
     parent :: __construct($id, $name, $dirName, $size,$newFileType, $newExtArray);
-    $this->setAltAttr($stringAlt);
+    $this->altString=$altString;
   }
 
   public function getAltAttr(){
     return $this->altString;
   }
-
   public function setAltAttr($string){
     $this->altString=$string;
   }
